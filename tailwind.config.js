dab18b3fae87a65e6597dd8d69e6453031363adc
Mohamed Aboalmkarem <146967816+Aboalmkarem@users.noreply.github.com>
2024-10-30 08:24:16 +0300

    auth page toggle arrow had been fixed

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      content: {
+        'link': 'url("/icons/link.svg")',
+      },
+    }
   },
   plugins: [require('daisyui')],
   daisyui: {
